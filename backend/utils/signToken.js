1b10e356a5b1b7344f6d34ff5495f52ec34b99a4
Samuel Ponce Luna <samuelponceluna@gmail.com>
2025-11-07 00:16:17 +0100

    commit 100 + add cors and update swagger :D

--- backend/utils/signToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-dotenv.config();
-const SECRET_KEY = process.env.JWT_SECRET;
-
-export default function signToken  (payload)  {
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
-  return token
-}
